ef10dd4fb56c77c40c2f39c77a1629a32a629747
Seth Near <seth.j.near@gmail.com>
2020-06-15 16:27:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
+  if name_hash.length == 0
+    return nil
+  else
+  values = name_hash.collect {|key, value| value}
+  smallest = values[0]
+     values.each { |value|
+       if value < smallest
+         smallest = value
+       end}
   name_hash.each {|key, value|
-    if hash.length == 0
-      return nil
-    else
-      smallest = name_hash.sort_by { |key, value|value }.first
-      return name_hash.key(smallest)
+    if value == smallest
+      return key
     end
   }
+       
 end
